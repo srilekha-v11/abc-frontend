887ba6c980d516d2f9654bd9606bf836ecc042cf
srilekha-v11 <srilekhav11@gmail.com>
2021-07-11 17:26:18 +0530

    styling 50%

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,9 @@
+import { WeekDay } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Complaint } from '../complaint';
+import { EngineerService } from '../view-main/services/engineer.service';
+
 
 @Component({
   selector: 'app-admin',
@@ -6,11 +11,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-
-  constructor() { }
+  complaintList: Complaint[] =[];
+  value: any;
+  
+  constructor(private engineerService:EngineerService,private router:Router) { }
 
   ngOnInit(): void {
   }
+  redirectComplaint(){
+    this.router.navigate(['/admin/listOfAllcomplaints']);
 
-  
+  }
+
 }
